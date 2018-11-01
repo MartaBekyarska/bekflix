d8d32ac966d52d5bb9550a71951955406608c1f9
Marta Bekyarska <martatsoneva@yahoo.com>
2018-11-01 13:03:07 +0000

    Positioned TV Shows titles in a grid layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
 import img from './img/Man-On-Fire.jpg';
+import BlackMirror from './img/BlackMirror.jpg';
+import BreakingBad from './img/BreakingBad.jpg';
+import DeathNote from './img/DeathNote.jpg';
+import GameOfThrones from './img/GameOfThrones.jpeg';
+import TheWalkingDead from './img/TheWalkingDead.jpg';
+import TheWire from './img/TheWire.jpg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-         <header>
-           <h2>
-             <ul>
-               <li>Black Mirror</li>
-               <li>Breaking Bad</li>
-               <li>Death Note</li>
-               <li>Game of Thrones</li>
-               <li>The Walking Death</li>
-               <li>The Wire</li>
-               </ul>
-               </h2>
-        <img src={img} className="App-logo" alt="cover" />
-          </header>
+    <div className="App">
+      <header>
+        <div className="table">
+          <div className="wrap">
+            <h2>Black Mirror</h2>
+          </div>
+          <div className="wrap">
+            <h2>Breaking Bad</h2>
+          </div>
+          <div className="wrap">
+            <h2>Death Note</h2>
+          </div>
+        </div>
         
+
+          <div className="table">
+            <div className="wrap">
+              <h2>Game of Thrones</h2>
+        
+            </div>
+            <div className="wrap">
+              <h2>The Walking Death</h2>
+            </div>
+            <div className="wrap">
+              <h2>The Wire</h2>
+            </div>
+          </div>
+          
+            
+        
+         {/* <img src={img} className="App-logo" alt="cover" />  */}
+        </header>
       </div>
     );
   }
