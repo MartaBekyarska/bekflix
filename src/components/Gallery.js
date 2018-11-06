c9d3b186df3b6ee0f15d1bd4517ce83e6ddac431
Marta Bekyarska <martatsoneva@yahoo.com>
2018-11-06 15:32:14 +0000

    Improved the layout of the images with flexbox and deleted media query #11

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -10,18 +10,13 @@ import CoverComponent from './Cover';
 
 const Gallery = () => {
   return (
-    <div>
-      <div className="table">
+    <div className="container">
         <CoverComponent id='blackmirror' title='BlackMirror' image={BlackMirror} />
         <CoverComponent id='breakingbad' title='BreakingBad' image={BreakingBad} />
         <CoverComponent id='deathnote' title='DeathNote' image={DeathNote} />
-      </div>
-
-      <div className="table">
         <CoverComponent id='gameofthrones' title='GameOfThrones' image={GameOfThrones} />
         <CoverComponent id='thewalkingdead' title='TheWalkingDead' image={TheWalkingDead} />
         <CoverComponent id='thewire' title='TheWire' image={TheWire} />
-      </div>
     </div>
 
   );
