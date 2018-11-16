1315212265aa89acd61f71a1f5ea61e1027d425b
Marta Bekyarska <martatsoneva@yahoo.com>
2018-11-16 12:56:00 +0000

    List of shows created in the backend #19

---
+++ src/back-end/shows.js
@@ -0,0 +1,50 @@
+
+
+const covers = [
+    {
+        id: 'blackmirror',
+        title: 'Black Mirror',
+        synopsis: `An anthology series exploring a twisted, high-tech world where humanity's greatest innovations and darkest instincts collide.`,
+        stars: `Stars: Daniel Lapaine, Hannah John-Kamen, Michaela Coel`, 
+        creator: 'Creator: Charlie Brooker'
+},
+    {
+        id: 'breakingbad', 
+        title: 'Breaking Bad', 
+        synopsis: `A “Breaking Bad” movie from original series creator Vince Gilligan is set to begin production in New Mexico later this month.
+
+    According to the Albuquerque Journal, the film will begin shooting under the title “Greenbrier” in mid-November and will run through February.
+    
+    The New Mexico film office told the Journal a film from Sony Pictures with such a title will begin shooting next month. The project is said to track the escape of a kidnapped man and his quest for freedom. The film office did not immediately respond to TheWrap’s request for comment.
+    ` },
+    {
+        id: 'deathnote', 
+        title: 'Death Note',  
+        synopsis: `A high school student named Light Turner discovers a mysterious notebook that has the power to kill anyone whose name is written within its pages, and launches a secret crusade to rid the world of criminals.`,
+        creator: `Director: Adam Wingard`,
+        stars: `Stars: Nat Wolff, Lakeith Stanfield, Margaret Qualley`
+    },
+    {
+        id: 'gameofthrones', 
+        title: 'Game Of Thrones',  
+        synopsis: `Nine noble families fight for control over the mythical lands of Westeros, while an ancient enemy returns after being dormant for thousands of years.`,
+        creator: `Creators: David Benioff, D.B. Weiss`,
+        stars: `Stars: Emilia Clarke, Peter Dinklage, Kit Harington`
+    },
+    {
+        id: 'thewalkingdead', 
+        title: 'The Walking Dead', 
+        synopsis: `Sheriff Deputy Rick Grimes wakes up from a coma to learn the world is in ruins, and must lead a group of survivors to stay alive.`,
+        creator: `Creators: Frank Darabont, Angela Kang`,
+        stars: `Stars: Andrew Lincoln, Norman Reedus, Melissa McBride `
+    },
+    {
+        id: 'thewire',
+        title: 'The Wire',  
+        synopsis: `Baltimore drug scene, seen through the eyes of drug dealers and law enforcement.`,
+        creator: `Creator: David Simon`,
+        stars: `Stars: Dominic West, Lance Reddick, Sonja Sohn` 
+    }
+];
+
+module.exports = covers;
