07c802e07fbb0c1ceb8a194e95a1d69acdc30578
Marta Bekyarska <martatsoneva@yahoo.com>
2018-11-26 14:31:08 +0000

    package.json files modified and added semicolon after variable assignment

--- src/back-end/app.js
+++ src/back-end/app.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require('express');
+const app = express();
+const port = 3001;
 const covers = require('./shows');
 
 app.get('/rest/shows', (req, res) => res.send(covers));
