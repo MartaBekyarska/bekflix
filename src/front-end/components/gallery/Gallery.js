adb2389cb7723f5b0ece60629a23a87b2ac5eccf
Marta Bekyarska <martatsoneva@yahoo.com>
2018-12-04 09:40:08 +0000

    Fixed the indentation and the code layout

--- src/front-end/components/gallery/Gallery.js
+++ src/front-end/components/gallery/Gallery.js
@@ -15,26 +15,30 @@ class Gallery extends React.Component {
 
   componentDidMount() {
     fetch('/rest/shows')
-    .then(response => {
-      console.log(response);
-      return response.json();
-    })
-    .then((covers) => { 
-      console.log(covers);
-      this.setState({ covers }); 
-    });
+      .then(response => {
+        console.log(response);
+        return response.json();
+      })
+      .then((covers) => {
+        console.log(covers);
+        this.setState({ covers });
+      });
   }
 
   render() {
     return (
-      <div className='container'> 
-      {   
-          covers.map(function(cover) {
-          return <CoverComponent id={cover.id} title={cover.title} image={cover.image} message={cover.message} />;
-        })
-      }
+      <div className='container'>
+        {
+          covers.map(function (cover) {
+            return <CoverComponent
+              id={cover.id}
+              title={cover.title}
+              image={cover.image}
+              message={cover.message} />;
+          })
+        }
       </div>
-  
+
     );
   }
 }
