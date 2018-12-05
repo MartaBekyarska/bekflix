487cd9f5bcf2d60efd9ad2c07758ca987795276c
Marta Bekyarska <martatsoneva@yahoo.com>
2018-12-05 19:44:56 +0000

    package.json modified and app.js file updated

--- src/back-end/app.js
+++ src/back-end/app.js
@@ -9,5 +9,8 @@ app.use(express.static(path.join(__dirname, '../../build')));
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, '../../build', 'index.html'));
 });
+// app.get('/', function(req, response) {
+//     response.send('hello world')
+// })
 
 app.listen(port, () => console.log(`Example app is listening!`));
