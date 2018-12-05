4a7bc9faf6be8fe64091a4f5b2fce5f261c044ac
Marta Bekyarska <martatsoneva@yahoo.com>
2018-12-05 12:36:17 +0000

    added path

--- src/back-end/app.js
+++ src/back-end/app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
 const covers = require('./shows');
+const path = require('path');
 const port = process.env.PORT || 3001;
 
-app.get('/rest/shows', (req, res) => res.send(covers))
+app.get('/rest/shows', (req, res) => res.send(covers));
 app.use(express.static(path.join(__dirname, '../../build')));
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, '../../build', 'index.html'));
 });
 
-app.listen(port, () => console.log(`Example app is listening!`))
+app.listen(port, () => console.log(`Example app is listening!`));
